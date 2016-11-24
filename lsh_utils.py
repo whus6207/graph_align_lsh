9ac17382630a81f7a95d120a0a8a82abc03c50bc
Kuan-Yu Chen <kyuchen@umich.edu>
2016-11-24 00:46:34 -0500

    Ranking and KL Divergence

--- lsh_utils.py
+++ lsh_utils.py
@@ -42,22 +42,30 @@ def cos_sim(v1, v2, scaling=None):
     v2 = np.multiply(v2, 1/scaling)
     return v1.dot(v2)/(np.linalg.norm(v1)*np.linalg.norm(v2))
 
+def KL_sim(v1, v2):
+    v1 = v1.astype(float) / np.mean(v1)
+    v2 = v2.astype(float) / np.mean(v2)
+    temp = np.log(v1 / v2)
+    temp[np.isinf(temp)] = 0
+    return np.sum(temp * v1, axis = 0)
+
+
 def computeMatchingMat(attributesA, attributesB, pair_count_dict):
     combineAB = selectAndCombine(attributesA, attributesB)
     matching_matrix = np.zeros((len(attributesA), len(attributesB)))
-    scale = np.mean(combineAB[:,1:], axis=0)
+    scale = np.mean(combineAB[:,2:], axis=0)
                 
     for pair, count in pair_count_dict.items():
-        matching_matrix[pair[0]][pair[1]-len(attributesA)] = cos_sim(combineAB[pair[0]][1:], combineAB[pair[1]][1:],scaling=scale)*count
+        matching_matrix[pair[0]][pair[1]-len(attributesA)] = cos_sim(combineAB[pair[0]][2:], combineAB[pair[1]][2:],scaling=scale)*count
         
     return matching_matrix
 
 def computeWholeSimMat(attributesA, attributesB):
     combineAB = selectAndCombine(attributesA, attributesB)
     sim_vec = []
-    scale = np.mean(combineAB[:,1:], axis=0)
+    scale = np.mean(combineAB[:,2:], axis=0)
     for j in range(len(attributesA)):
-        vec = [cos_sim(combineAB[j,1:], combineAB[len(attributesA)+i,1:], scale) for i in range(len(attributesB)) ]
+        vec = [cos_sim(combineAB[j,2:], combineAB[len(attributesA)+i,2:], scale) for i in range(len(attributesB)) ]
         sim_vec.append(vec)
 
     return np.array(sim_vec)
@@ -73,7 +81,7 @@ def combineBucketsBySum(buckets, combineAB, Afname):
             B_idx = ~A_idx
             colli_arr = np.array(collisions)
             
-            if sum(A_idx) == len(A_idx):
+            if sum(A_idx) == len(A_idx):    # Not all in A 
                 continue
                 
             for aid in colli_arr[A_idx]:
@@ -82,10 +90,23 @@ def combineBucketsBySum(buckets, combineAB, Afname):
 
     return pair_count_dict
 
+
 def plotBucketDistribution(bucket):
     plt.bar(range(len(bucket))
             , sorted([len(values) for key, values in bucket.items()])
             , align='center')
     plt.show()
 
+
+def Rank(matching_matrix, P = None):
+    if P is not None:
+        matching_matrix = matching_matrix.dot(P)
+    n, d = matching_matrix.shape
+    
+    ranking = np.zeros((n))
+    for i in range(n):      
+        #rank = n - matching_matrix[i, :].argsort().tolist().index(i)
+        rank = sorted(matching_matrix[i, :], reverse = True).index(matching_matrix[i, i]) + 1
+        ranking[i] = 1.0 / rank
+    return ranking
 
